9dda29c269ddb9328609eeae6533356430b8d053
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-30 15:44:13 -0500

    Add image, text to Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,24 @@
 <template>
-  <section></section>
+  <section class="hero md-layout-item">
+    <picture>
+      <img src="../assets/images/F-210_shadowed.png" alt="F-210 Drone Image" class="background-image">
+    </picture>
+    <article class="body">
+      <h1 class="md-display-4">F-210</h1>
+      <h2 class="md-display-1">"The Fastest Drone on the Planet"</h2>
+      <p class="md-subheading">The Wirecutter</p>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Hero',
-  props: {}
+  props: {},
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
+    }
+  }
 }
 </script>
