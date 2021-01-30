899576eef829db3821cc6dd1a856bed4567e5d3e
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-30 16:27:23 -0500

    Edit classes on hero image, typography

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,16 @@
 <template>
   <section class="hero md-layout-item">
     <picture>
-      <img src="../assets/images/F-210_shadowed.png" alt="F-210 Drone Image" class="background-image">
+      <img
+        src="../assets/images/F-210_shadowed.png"
+        alt="F-210 Drone Image"
+        class="background-image"
+      >
     </picture>
     <article class="body">
-      <h1 class="md-display-4">F-210</h1>
-      <h2 class="md-display-1">"The Fastest Drone on the Planet"</h2>
-      <p class="md-subheading">The Wirecutter</p>
+      <h1 class="jumbotron">F-210</h1>
+      <h2 class="quote-body">"The Fastest Drone on the Planet"</h2>
+      <p class="quote-source">&ndash;The Wirecutter</p>
     </article>
   </section>
 </template>
