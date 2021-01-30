6e2c28f2529a83b098d78f401b7668e1b8fecdf7
Catherine Kleimeier <catherine@masugadesign.com>
2021-01-30 15:43:28 -0500

    Import main stylesheet into application

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import './assets/styles/styles.scss'
 
 createApp(App).mount('#app')
